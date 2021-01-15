d40422eaf5fbea03ec6e577189067fe4c868ec8c
inquityy1 <nebojsanikolic12350@yahoo.com>
2021-01-15 16:13:35 +0100

    some update console.log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -105,10 +105,15 @@ class ProjectItem {
   }
 
 	connectDrag() {
-		document.getElementById(this.id).addEventListener('dragstart', event => {
+		const item = document.getElementById(this.id);
+		item.addEventListener('dragstart', event => {
 			event.dataTransfer.setData('text/plain', this.id);
 			event.dataTransfer.effectAllowed = 'move';
 		});
+		
+		item.addEventListener('dragend', event => {
+			console.log(event);
+		})
 	}
 
   connectMoreInfoButton() {
